1a74fa69f2ac21eb9f6fee820eea98952ec789a9
Hemanta Gayen <69314427+hemucode@users.noreply.github.com>
2023-02-21 10:56:23 +0530

    gf

--- Chrome/data/interface/index.js
+++ Chrome/data/interface/index.js
@@ -1,3 +1,32 @@
+//================================================
+/*
+MIT License
+
+Fullscreen, #1 Zoom and Dark Mode more options will get in one
+Copyright (C) 2023 hemanta gayen
+www.downloadhub.cloud
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+//================================================
+
 domReady(() => {
    translateTITLE()
    ZoomFun()
@@ -6,7 +35,6 @@ domReady(() => {
 /**
  * @returns Codehemu
  */
-
 
 function domReady (callback) {
   if (document.readyState === 'complete') {
@@ -23,7 +51,7 @@ function domReady (callback) {
 function translateTITLE (dataKey = 'message') {
   for (const $element of document.getElementsByTagName('*')) {
     if ($element.dataset && $element.dataset[dataKey]) {
-      $element.innerText = chrome.i18n.getMessage($element.dataset[dataKey])
+      $element.title = chrome.i18n.getMessage($element.dataset[dataKey])
     }
   }
 }
